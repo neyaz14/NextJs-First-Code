8c0d2ee41005feb1864a6095ddf0589a2859c850
Neyaz Morshid <niyazmorshid@gmail.com>
2025-02-02 10:11:58 +0600

    notfound page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const notfound = () => {
+    return (
+        <div>
+            <p>The page does not found </p>
+        </div>
+    );
+};
+
+export default notfound;
